4056f1861ab43804addaf467ff0a14c54d8d6c0b
Domício Medeiros <domicioam@gmail.com>
2021-03-21 12:55:23 +0000

    Creating class for BankActor

---
+++ MoneyTransactionsTests/Actors/BankActorTest.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using Akka.TestKit.Xunit2;
+using MoneyTransactions.Actors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MoneyTransactionsTests.Actors
+{
+    public class BankActorTest : TestKit
+    {
+        [Fact]
+        public void Should_create_new_account()
+        {
+            var subject = Sys.ActorOf(Props.Create(() => new BankActor()));
+        }
+    }
+}
